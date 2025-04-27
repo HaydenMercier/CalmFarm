e4a0979658fedb05d6ca7ae81c6f3f23ec678f3f
Hayden Mercier <ultra.archerhm@gmail.com>
2025-04-27 12:52:38 +1000

    Created a working UI for using and changing tool types

---
+++ calmfarm/Scenes/ui/tool_panel.gd
@@ -0,0 +1,39 @@
+extends PanelContainer
+
+@onready var tool_axe: Button = $MarginContainer/HBoxContainer/ToolAxe
+@onready var tool_tilling: Button = $MarginContainer/HBoxContainer/ToolTilling
+@onready var tool_watering_can: Button = $MarginContainer/HBoxContainer/ToolWateringCan
+@onready var tool_corn: Button = $MarginContainer/HBoxContainer/ToolCorn
+@onready var tool_tomato: Button = $MarginContainer/HBoxContainer/ToolTomato
+
+
+func _on_tool_axe_pressed() -> void:
+	ToolManager.select_tool(DataTypes.Tools.AxeWood)
+	print("axe")
+
+func _on_tool_tilling_pressed() -> void:
+	ToolManager.select_tool(DataTypes.Tools.TillGround)
+	print("till")
+
+func _on_tool_watering_can_pressed() -> void:
+	ToolManager.select_tool(DataTypes.Tools.WaterCrops)
+	print("water")
+
+
+func _on_tool_corn_pressed() -> void:
+	ToolManager.select_tool(DataTypes.Tools.PlantCorn)
+	print("corn")
+
+
+func _on_tool_tomato_pressed() -> void:
+	ToolManager.select_tool(DataTypes.Tools.PlantTomato)
+	print("tomato")
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("release_tool"):
+		ToolManager.select_tool(DataTypes.Tools.None)
+		tool_axe.release_focus()
+		tool_tilling.release_focus()
+		tool_watering_can.release_focus()
+		tool_corn.release_focus()
+		tool_tomato.release_focus()
